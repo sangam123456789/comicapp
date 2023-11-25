20bd0e56c63d0f789872f43ec229378c6fefc01e
sangam123456789 <91962643+sangam123456789@users.noreply.github.com>
2023-11-26 01:46:01 +0530

    hoja

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
-import Navbar  from './components/Navbar';
-import Form from './components/Form';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Form from "./components/Form";
 
 function App() {
   return (
-    <div className="App">
-        <Navbar />
-        <Form />
+    <div
+      className="App"
+      style={{ backgroundColor: "black", minHeight: "700px" }}
+    >
+      <Navbar />
+      <Form />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import { Button } from "@mui/material";
+import { spiral } from "ldrs";
+import { waveform } from "ldrs";
+waveform.register();
+spiral.register();
 
 async function query(data) {
   const response = await fetch(
@@ -6,7 +11,8 @@ async function query(data) {
     {
       headers: {
         Accept: "image/png",
-        Authorization: "Bearer VknySbLLTUjbxXAXCjyfaFIPwUTCeRXbFSOjwRiCxsxFyhbnGjSFalPKrpvvDAaPVzWEevPljilLVDBiTzfIbWFdxOkYJxnOPoHhkkVGzAknaOulWggusSFewzpqsNWM",
+        Authorization:
+          "Bearer VknySbLLTUjbxXAXCjyfaFIPwUTCeRXbFSOjwRiCxsxFyhbnGjSFalPKrpvvDAaPVzWEevPljilLVDBiTzfIbWFdxOkYJxnOPoHhkkVGzAknaOulWggusSFewzpqsNWM",
         "Content-Type": "application/json",
       },
       method: "POST",
@@ -18,7 +24,11 @@ async function query(data) {
 }
 
 function ComicPanelForm() {
-  const initialPanelData = Array(10).fill({ text: "", imageSrc: null, isLoading: false });
+  const initialPanelData = Array(10).fill({
+    text: "",
+    imageSrc: null,
+    isLoading: false,
+  });
   const [panelData, setPanelData] = useState(initialPanelData);
 
   const handleInputChange = (index, value) => {
@@ -59,45 +69,90 @@ function ComicPanelForm() {
 
   return (
     <div>
-      <h2 style={{ marginTop: "30px", textAlign: "center" }}>Comic Panel Input</h2>
-      <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-between" }}>
+      <h2
+        style={{
+          marginTop: "30px",
+          marginBottom: "20px",
+          textAlign: "center",
+          color: "#88cc78",
+        }}
+      >
+        Create Comic
+      </h2>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+        }}
+      >
         {panelData.map((panel, index) => (
-          <div key={index} style={{  marginBottom: "30px", width: "48%" }}>
-            
-            <div style={{marginLeft: "30px", width: "50%"}}>
-                <input
+          <div
+            key={index}
+            style={{
+              backgroundColor: "#e4e4e4",
+              margin: "0px 0px 20px 40px",
+              width: "45%",
+              border: "2px solid rgb(59 183 140)",
+              borderRadius: "5px",
+            }}
+          >
+            <div
+              style={{
+                margin: "10px 0px 0px 30px",
+                width: "50%",
+                boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.15)",
+              }}
+            >
+              <input
                 type="text"
                 id={`panelText${index + 1}`}
                 className="form-control"
                 placeholder={`Enter image name`}
                 value={panel.text}
                 onChange={(e) => handleInputChange(index, e.target.value)}
-                style={{marginRight: "20px" }}
-                />
+                style={{ marginRight: "20px" }}
+              />
             </div>
-            <div style={{width: "30%", margin: "-40px 0px 0px 389px"}}>
-                <button
+            <div style={{ width: "20%", margin: "-35px 0px 0px 450px" }}>
+              <Button
                 type="button"
-                className="btn btn-primary"
+                // className="btn btn-primary"
                 onClick={() => handleSubmit(index)}
-                >
-                Submit Panel {index + 1}
-                </button>
+                style={{
+                  color: "black",
+                  backgroundColor: "#309e65",
+                  height: "30px",
+                  padding: "1rem",
+                }}
+                variant="contained"
+              >
+                Comic {index + 1}
+              </Button>
             </div>
-            {panel.isLoading && <p>Loading...</p>}
-            <div style={{
-                    marginTop: "20px", 
-                    marginLeft: "30px",  
-                    maxWidth: "80%",
-                    border: "1px solid black", 
-                }}>
-                {panel.imageSrc && (
+            <div
+              style={{
+                height: "30px",
+                width: "10%",
+                margin: "-26px -138px 1px 321px",
+              }}
+            >
+              {panel.isLoading && <l-waveform size="19"></l-waveform>}
+            </div>
+            <div
+              style={{
+                marginTop: "20px",
+                marginLeft: "0px",
+                maxWidth: "100%",
+                border: "1px solid black",
+              }}
+            >
+              {panel.imageSrc && (
                 <img
-                    src={panel.imageSrc}
-                    alt={`Generated Image for Panel ${index + 1}`}
-                    style={{padding: "0px"}}
+                  src={panel.imageSrc}
+                  alt={`Generated Image for Panel ${index + 1}`}
+                  style={{ padding: "0px", width: "100%" }}
                 />
-                )}
+              )}
             </div>
           </div>
         ))}
